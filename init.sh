bc14e5f89c6f2e53f6fa1085d47aceac06fe1fbb
simondgreenhill <simondgreenhill@gmail.com>
2019-04-20 07:58:38 -0700

    Switch to installing zsh and ohmyzsh manually

--- init.sh
+++ init.sh
@@ -2,17 +2,22 @@
 
 DIR=$(cd `dirname $0` && pwd)
 
-# install ohmyzsh if not installed 
-if ! [[-f "~/.oh-my-zsh"]]; then
-    sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# warn about zsh and ohmyzsh if not installed 
+if ! [[ -f "~/.zsh_history" ]]; then
+   echo "WARNING: Zsh not installed."
 fi
 
+if ! [[ -f "~/.oh-my-zsh" ]]; then
+    echo "WARNING: ohmyzsh not installed."
+else 
+     # if zsh and ohmyzsh installed, run setup. 
+    ln -sf $DIR/zsh/zshrc ~/.zshrc
+    source ~/.zshrc
+fi
+
+# create symlinks
 # ln -sf $DIR/bash/profile ~/.bash_profile
 ln -sf $DIR/git/gitconfig ~/.gitconfig
 ln -sf $DIR/vim/vimrc ~/.vimrc
-ln -sf $DIR/zsh/zshrc ~/.zshrc
-
-# source the zsh profile
-source ~/.zshrc
 
 
